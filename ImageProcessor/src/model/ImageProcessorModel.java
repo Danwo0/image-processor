06f48dcbf163edb2aad7a62bd5a083548ac25529
Jun Lee <dan.junwoo.lee@gmail.com>
2021-10-28 19:18:33 -0400

    Initial setup

---
+++ ImageProcessor/src/model/ImageProcessorModel.java
@@ -0,0 +1,9 @@
+public interface ImageProcessorModel {
+  public void brighten (int amount);
+
+  public void flipVertical();
+
+  public void flipHorizontal();
+
+  public void greyscale(String mode);
+}
---
+++ ImageProcessor/src/view/ImageProcessorView.java
@@ -0,0 +1,5 @@
+public interface ImageProcessorView {
+
+  public void outputImage();
+
+}
