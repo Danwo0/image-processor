27753d520a0cb878d405a96f0400c309d551f717
laibri <90637834+laibri@users.noreply.github.com>
2021-11-02 02:04:36 -0400

    move test

--- ImageProcessor/ test/ImageProcessorModelImplTest.java
+++ ImageProcessor/ test/ImageProcessorModelImplTest.java
@@ -1,6 +1,7 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -21,29 +22,29 @@ public class ImageProcessorModelImplTest {
 
   @Test(expected = IllegalArgumentException.class)
   public void loadNonPPM() {
-    model.loadImage("./res/P2.ppm", "haha");
+    model.loadImage("ImageProcessor/res/P2.ppm", "haha");
   }
 
   @Test(expected = IllegalArgumentException.class)
   public void load404() {
-    model.loadImage("./res/ThisFileDoesnt.Exist", "haha");
+    model.loadImage("ImageProcessor/res/ThisFileDoesnt.Exist", "haha");
   }
 
   @Test
   public void testLoad() {
     String actual = "";
     try {
-      actual = Files.readString(Paths.get("./res/pix.ppm"));
+      actual = Files.readString(Paths.get("ImageProcessor/res/pix.ppm"));
     } catch (IOException e) {
       e.printStackTrace();
     }
 
-    model.loadImage("./res/pix.ppm", "Test");
+    model.loadImage("ImageProcessor/res/pix.ppm", "Test");
     assertEquals(model.saveImage("Test"), actual);
   }
 
   @Test(expected = IllegalArgumentException.class)
   public void saveNotInMap() {
-
+    model.saveImage("DoesntExist");
   }
 }
