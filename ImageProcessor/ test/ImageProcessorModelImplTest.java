79dfb95f635441cd4bc2bb28d7a6759e364d62fa
laibri <90637834+laibri@users.noreply.github.com>
2021-11-02 03:19:25 -0400

    update teest

--- ImageProcessor/ test/ImageProcessorModelImplTest.java
+++ ImageProcessor/ test/ImageProcessorModelImplTest.java
@@ -13,11 +13,44 @@ import static org.junit.Assert.assertEquals;
 
 public class ImageProcessorModelImplTest {
 
-  ImageProcessorModel model;
-
+  private ImageProcessorModel model;
+  private String pix;
+  private String pix2;
+  private String pixBrighter;
+  private String pixDarker;
+  private String pixVertical;
+  private String pixHorizontal;
+  private String pixGreyValue;
+  private String pixGreyR;
+  private String pixGreyG;
+  private String pixGreyB;
+  private String pixGreyLuma;
+  private String pixGreyIntensity;
   @Before
   public void setUp() {
     model = new ImageProcessorModelImpl();
+
+    try {
+      pix = Files.readString(Paths.get("./res/pix.ppm"));
+      pix2 = Files.readString(Paths.get("./res/pix2.ppm"));
+      pixBrighter = Files.readString(Paths.get("./res/pixBrighter.ppm"));
+      pixDarker = Files.readString(Paths.get("./res/pixDarker.ppm"));
+      pixVertical = Files.readString(Paths.get("./res/pixVertical.ppm"));
+      pixHorizontal = Files.readString(Paths.get("./res/pixHorizontal.ppm"));
+      pixGreyValue = Files.readString(Paths.get("./res/pixGreyValue.ppm"));
+      pixGreyR = Files.readString(Paths.get("./res/pixGreyR.ppm"));
+      pixGreyG = Files.readString(Paths.get("./res/pixGreyG.ppm"));
+      pixGreyB = Files.readString(Paths.get("./res/pixGreyB.ppm"));
+      pixGreyLuma = Files.readString(Paths.get("./res/pixGreyLuma.ppm"));
+      pixGreyIntensity = Files.readString(Paths.get("./res/pixGreyIntensity.ppm"));
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void resetModel() {
+    model = new ImageProcessorModelImpl();
+    model.loadImage("./res/pix.ppm", "Test");
   }
 
   @Test(expected = IllegalArgumentException.class)
@@ -57,69 +90,57 @@ public class ImageProcessorModelImplTest {
 
   @Test
   public void testLoad() {
-    model = new ImageProcessorModelImpl();
-    String actual = "";
-    try {
-      actual = Files.readString(Paths.get("./res/pix.ppm"));
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-
-    model.loadImage("./res/pix.ppm", "Test");
-    assertEquals(model.saveImage("Test"), actual);
+    resetModel();
+    assertEquals(model.saveImage("Test"), pix);
   }
 
   @Test
   public void testOverwrite() {
-    model = new ImageProcessorModelImpl();
-    String pix = "";
-    String pix2 = "";
-    try {
-      pix = Files.readString(Paths.get("./res/pix.ppm"));
-      pix2 = Files.readString(Paths.get("./res/pix2.ppm"));
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
+    resetModel();
 
-    model.loadImage("./res/pix.ppm", "Test");
-    assertEquals(model.saveImage("Test"), pix);
     model.loadImage("./res/pix2.ppm", "Test2");
-    assertEquals(model.saveImage("Test2"), pix2);
     model.loadImage("./res/pix2.ppm", "Test");
+    assertEquals(model.saveImage("Test2"), pix2);
     assertEquals(model.saveImage("Test"), pix2);
   }
 
   @Test
   public void testBrighten() {
-    model = new ImageProcessorModelImpl();
-    String pix = "";
-    try {
-      pix = Files.readString(Paths.get("./res/pixBrighter.ppm"));
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
+    resetModel();
 
-    model.loadImage("./res/pix.ppm", "Test");
     model.changeBrightness("Test", "TestOut", 1);
     model.changeBrightness("Test", "Test", 1);
-    assertEquals(model.saveImage("TestOut"), pix);
-    assertEquals(model.saveImage("Test"), pix);
+    assertEquals(model.saveImage("TestOut"), pixBrighter);
+    assertEquals(model.saveImage("Test"), pixBrighter);
   }
 
   @Test
   public void testDarken() {
-    model = new ImageProcessorModelImpl();
-    String pix = "";
-    try {
-      pix = Files.readString(Paths.get("./res/pixDarker.ppm"));
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
+    resetModel();
 
-    model.loadImage("./res/pix.ppm", "Test");
     model.changeBrightness("Test", "TestOut", -1);
     model.changeBrightness("Test", "Test", -1);
-    assertEquals(model.saveImage("TestOut"), pix);
-    assertEquals(model.saveImage("Test"), pix);
+    assertEquals(model.saveImage("TestOut"), pixDarker);
+    assertEquals(model.saveImage("Test"), pixDarker);
+  }
+
+  @Test
+  public void testVertical() {
+    resetModel();
+
+    model.flipVertical("Test", "TestOut");
+    model.flipVertical("Test", "Test");
+    assertEquals(model.saveImage("TestOut"), pixVertical);
+    assertEquals(model.saveImage("Test"), pixVertical);
+  }
+
+  @Test
+  public void testHorizontal() {
+    resetModel();
+
+    model.changeBrightness("Test", "TestOut", -1);
+    model.changeBrightness("Test", "Test", -1);
+    assertEquals(model.saveImage("TestOut"), pixHorizontal);
+    assertEquals(model.saveImage("Test"), pixHorizontal);
   }
 }
