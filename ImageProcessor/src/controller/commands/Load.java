9d18a619d627732d252152b0574c19141ff50b1f
Jun Lee <dan.junwoo.lee@gmail.com>
2021-11-08 18:22:02 -0500

    Save and load in controller lol

--- ImageProcessor/src/controller/commands/Load.java
+++ ImageProcessor/src/controller/commands/Load.java
@@ -1,5 +1,13 @@
 package controller.commands;
 
+import java.awt.image.BufferedImage;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import javax.imageio.ImageIO;
+
 import model.ImageProcessorModel;
 
 /**
@@ -21,12 +29,31 @@ public class Load extends AbstractCommand {
 
   @Override
   public void complete(ImageProcessorModel m) {
+    String format = fileName.substring(fileName.lastIndexOf(".") + 1);
+    if (format.equalsIgnoreCase("ppm")) {
+      completePPM(m);
+    } else {
+      BufferedImage loadedImage = null;
+      try {
+        loadedImage = ImageIO.read(new FileInputStream(fileName));
+        m.loadImage(loadedImage, imageName);
+      } catch (IllegalArgumentException e) {
+        message = "Given path is invalid." + System.lineSeparator();
+      } catch (IOException e) {
+        message = "Failed to read from the path" + System.lineSeparator();
+      }
+    }
+  }
+
+  private void completePPM(ImageProcessorModel m) {
     try {
-      m.loadImage(fileName, imageName);
+      m.loadPPM(Files.readString(Paths.get("res/controllerSaveTest.ppm")), imageName);
       message = "Successfully loaded " + fileName + " as "
               + imageName + "." + System.lineSeparator();
     } catch (IllegalArgumentException e) {
-      message = "Given filename does not exist!" + System.lineSeparator();
+      message = "Given invalid filename!" + System.lineSeparator();
+    } catch (IOException e) {
+      message = "Failed to read from the path!" + System.lineSeparator();
     }
   }
 }
