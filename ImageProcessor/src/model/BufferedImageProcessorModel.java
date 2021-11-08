135f90f4c780a447cbf687cbbd78c96eed7fcf57
laibri <90637834+laibri@users.noreply.github.com>
2021-11-08 02:01:11 -0500

    bufferedimage

--- ImageProcessor/src/model/BufferedImageProcessorModel.java
+++ ImageProcessor/src/model/BufferedImageProcessorModel.java
@@ -1,18 +1,17 @@
 package model;
 
-import java.awt.Image;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Scanner;
 
 import javax.imageio.ImageIO;
 
 public class BufferedImageProcessorModel implements ImageProcessorModel {
-  Map<String, Image> images;
+  Map<String, BufferedImage> images;
 
   public BufferedImageProcessorModel() {
     this.images = new HashMap<>();
@@ -20,7 +19,7 @@ public class BufferedImageProcessorModel implements ImageProcessorModel {
 
   @Override
   public void loadImage(String fileName, String imageName) throws IllegalArgumentException {
-    Image loadedImage;
+    BufferedImage loadedImage;
 
     try {
       loadedImage = ImageIO.read(new FileInputStream(fileName));
@@ -33,27 +32,138 @@ public class BufferedImageProcessorModel implements ImageProcessorModel {
 
   @Override
   public void saveImage(String fileName, String imageName) throws IllegalArgumentException {
-    if (!ImageIO.write(images.get(imageName)))
-    return null;
+    String format = fileName.substring(fileName.lastIndexOf(".") + 1);
+    try {
+      if (!ImageIO.write(images.get(imageName), format, new File(fileName))) {
+        throw new IllegalArgumentException("Write failed");
+      }
+    } catch (IOException e) {
+      throw new IllegalArgumentException(e.getMessage());
+    }
   }
 
   @Override
   public void changeBrightness(String in, String out, int amount) throws IllegalArgumentException {
+    BufferedImage output = images.get(in);
+    if (output == null) {
+      throw new IllegalArgumentException("Image name is invalid");
+    }
 
+    for (int i = output.getMinX(); i < output.getWidth(); i++) {
+      for (int j = output.getMinY(); i < output.getHeight(); i++) {
+        Color color = new Color(output.getRGB(i, j));
+        output.setRGB(i, j, new Color(color.getRed() + amount,
+                color.getGreen() + amount,
+                color.getBlue() + amount).getRGB());
+      }
+    }
+    images.put(out, output);
   }
 
   @Override
   public void flipVertical(String in, String out) {
+    BufferedImage output = images.get(in);
+    if (output == null) {
+      throw new IllegalArgumentException("Image name is invalid");
+    }
 
+    for (int i = output.getMinY(); i < output.getHeight(); i++) {
+      output[image.length - 1 - i] = image[i];
+    }
+
+    images.put(out, output);
   }
 
   @Override
   public void flipHorizontal(String in, String out) {
+    BufferedImage output = images.get(in);
+    if (output == null) {
+      throw new IllegalArgumentException("Image name is invalid");
+    }
 
+    for (int i = output.getMinX(); i < output.getWidth(); i++) {
+      for (int j = output.getMinY(); i < output.getHeight(); i++) {
+        output[i][image[i].length - 1 - j] = image[i][j];
+      }
+    }
+
+    images.put(out, output);
   }
 
   @Override
   public void greyscale(String in, String out, GreyscaleMode mode) {
+    int[][][] image;
 
+    image = images.get(in);
+    if (image == null) {
+      throw new IllegalArgumentException("Image name is invalid");
+    }
+
+    int[][][] output = new int[image.length][image[0].length][3];
+
+    switch (mode) {
+      case Value:
+        for (int i = 0; i < image.length; i++) {
+          for (int j = 0; j < image[i].length; j++) {
+            for (int k = 0; k < image[i][j].length; k++) {
+              output[i][j][k] = Math.max(Math.max(image[i][j][0], image[i][j][1]), image[i][j][2]);
+            }
+          }
+        }
+        break;
+      case ValueR:
+        for (int i = 0; i < image.length; i++) {
+          for (int j = 0; j < image[i].length; j++) {
+            for (int k = 0; k < image[i][j].length; k++) {
+              output[i][j][k] = image[i][j][0];
+            }
+          }
+        }
+        break;
+      case ValueG:
+        for (int i = 0; i < image.length; i++) {
+          for (int j = 0; j < image[i].length; j++) {
+            for (int k = 0; k < image[i][j].length; k++) {
+              output[i][j][k] = image[i][j][1];
+            }
+          }
+        }
+        break;
+      case ValueB:
+        for (int i = 0; i < image.length; i++) {
+          for (int j = 0; j < image[i].length; j++) {
+            for (int k = 0; k < image[i][j].length; k++) {
+              output[i][j][k] = image[i][j][2];
+            }
+          }
+        }
+        break;
+      case Intensity:
+        for (int i = 0; i < image.length; i++) {
+          for (int j = 0; j < image[i].length; j++) {
+            for (int k = 0; k < image[i][j].length; k++) {
+              output[i][j][k] = (image[i][j][0] + image[i][j][1] + image[i][j][2]) / 3;
+            }
+          }
+        }
+        break;
+      case Luma:
+        for (int i = 0; i < image.length; i++) {
+          for (int j = 0; j < image[i].length; j++) {
+            for (int k = 0; k < image[i][j].length; k++) {
+              output[i][j][k] = (int) (0.2126 * image[i][j][0] + 0.7152 * image[i][j][1] + 0.0722
+                      * image[i][j][2]);
+            }
+          }
+        }
+        break;
+      default:
+        output = image;
+        break;
+    }
+
+    images.put(out, output);
+    maxValue.put(out, maxValue.get(in));
+  }
   }
 }
